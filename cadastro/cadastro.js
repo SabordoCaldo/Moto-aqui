5d514f1d5d4cbc5667c6b87dd2b8a43310d5eff9
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-19 14:59:45 -0300

    Create cadastro.js

---
+++ cadastro/cadastro.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+
+<link rel="stylesheet" href="../css/style.css">
+
+</head>
+
+<body>
+
+<div class="container">
+  <div class="card">
+
+    <h1>Cadastro Cliente</h1>
+
+    <div class="form">
+
+      <label>Nome</label>
+      <div class="input-box">
+        <input id="nome">
+      </div>
+
+      <label>Email</label>
+      <div class="input-box">
+        <input id="email">
+      </div>
+
+      <label>Senha</label>
+      <div class="input-box">
+        <input id="senha">
+      </div>
+
+      <button class="btn" onclick="cadastrarCliente()">Cadastrar</button>
+
+    </div>
+
+  </div>
+</div>
+
+<script src="../js/cadastro.js"></script>
+
+</body>
+</html>
