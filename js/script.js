ecc98749e48cf3860ce276cc040c5827d83a05e4
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-19 00:46:39 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,13 +6,30 @@ SCRIPT
 function cadastrarCliente(){
 
   const nome = document.getElementById("nome").value;
+  const telefone = document.getElementById("telefone").value;
+  const endereco = document.getElementById("endereco").value;
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
+  const confirmarSenha = document.getElementById("confirmarSenha").value;
+
+  const usuarioExistente = JSON.parse(localStorage.getItem("usuario"));
+
+  if(usuarioExistente && usuarioExistente.email === email){
+    alert("Esse e-mail já está cadastrado!");
+    return;
+  }
+
+  if(senha !== confirmarSenha){
+    alert("As senhas não coincidem!");
+    return;
+  }
 
   const usuario = {
-    nome: nome,
-    email: email,
-    senha: senha,
+    nome,
+    telefone,
+    endereco,
+    email,
+    senha,
     tipo: "cliente"
   };
 
@@ -32,9 +49,9 @@ function cadastrarMotorista(){
   const senha = document.getElementById("senha").value;
 
   const usuario = {
-    nome: nome,
-    email: email,
-    senha: senha,
+    nome,
+    email,
+    senha,
     tipo: "motorista"
   };
 
@@ -55,38 +72,46 @@ function login(){
   const usuario = JSON.parse(localStorage.getItem("usuario"));
 
   if(!usuario){
-    alert("Usuário não cadastrado");
+    alert("Usuário não cadastrado!");
     return;
   }
 
   if(email === usuario.email && senha === usuario.senha){
 
     if(usuario.tipo === "cliente"){
-      window.location.href = "cliente-home.html";
+      window.location.href = "cadastro/cliente-home.html";
     }else{
-      window.location.href = "motorista-home.html";
+      window.location.href = "cadastro/motorista.html";
     }
 
   }else{
-    alert("Email ou senha inválidos");
+    alert("Email ou senha inválidos!");
   }
-        }
+}
+
 
 // PREVIEW DA FOTO
 document.addEventListener("DOMContentLoaded", () => {
+
   const fotoInput = document.getElementById("foto");
   const preview = document.getElementById("preview");
 
   if(fotoInput){
     fotoInput.addEventListener("change", function(){
+
       const file = this.files[0];
+
       if(file){
         const reader = new FileReader();
+
         reader.onload = function(e){
           preview.src = e.target.result;
         }
+
         reader.readAsDataURL(file);
       }
+
     });
   }
+
 });
