33ccceeffeb68dd2ccfb07cb0a2c561946932f3f
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-19 15:33:40 -0300

    Create termos.js

---
+++ termos/termos.js
@@ -0,0 +1,38 @@
+const termosApp = {
+  versao: "1.0",
+  data: "19/04/2026",
+
+  texto: `
+<b>TERMOS DE USO – MOTO AQUI</b><br><br>
+
+Última atualização: 19/04/2026<br><br>
+
+Ao utilizar o aplicativo Moto Aqui, você concorda com os seguintes termos:<br><br>
+
+1. O Moto Aqui atua como intermediador entre clientes e motoristas, conectando usuários para realização de corridas.<br><br>
+
+2. O usuário é responsável pelas informações fornecidas no cadastro, devendo mantê-las corretas, atualizadas e verdadeiras.<br><br>
+
+3. O uso do aplicativo deve ser feito de forma ética, respeitando outros usuários e a legislação vigente.<br><br>
+
+4. É proibido utilizar o aplicativo para atividades ilegais, fraudulentas ou que prejudiquem terceiros.<br><br>
+
+5. O Moto Aqui não se responsabiliza por atrasos, cancelamentos, condutas ou quaisquer ocorrências entre clientes e motoristas.<br><br>
+
+6. Os valores, condições e negociações das corridas são de responsabilidade dos usuários envolvidos.<br><br>
+
+7. O aplicativo poderá suspender ou excluir contas que violem estes termos, sem aviso prévio.<br><br>
+
+8. Os dados fornecidos poderão ser utilizados para funcionamento do aplicativo e melhorias do serviço.<br><br>
+
+9. O Moto Aqui não comercializa dados pessoais, salvo quando necessário para funcionamento do serviço ou obrigação legal.<br><br>
+
+10. O uso do aplicativo depende de acesso à internet e dispositivo compatível, sendo responsabilidade do usuário.<br><br>
+
+11. O aplicativo pode apresentar instabilidades ou indisponibilidade temporária.<br><br>
+
+12. Estes termos podem ser atualizados a qualquer momento, sendo responsabilidade do usuário consultá-los periodicamente.<br><br>
+
+13. Ao continuar, você declara que leu, compreendeu e concorda com todos os termos apresentados.
+`
+};
