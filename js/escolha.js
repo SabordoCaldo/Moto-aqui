51ddf4aeac27ff4037f44f74c4a024a0ecd46828
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-19 16:31:29 -0300

    Update escolha.js

--- js/escolha.js
+++ js/escolha.js
@@ -1,30 +1,70 @@
-let tipoSelecionado = null;
+function getArquivoCidade(cidade){
 
-function selecionarTipo(tipo){
-  tipoSelecionado = tipo;
-  validar();
-}
+  switch(cidade){
 
-function validar(){
-  const cidade = document.getElementById("cidade").value;
-  const termos = document.getElementById("termosCheck").checked;
-  const btn = document.getElementById("btnContinuar");
+    case "Passos":
+      return "../cidades/passos.js";
 
-  if(tipoSelecionado && cidade && termos){
-    btn.disabled = false;
-  }else{
-    btn.disabled = true;
+    case "Alpinópolis":
+      return "../cidades/alpinopolis.js";
+
+    case "Bom Jesus da Penha":
+      return "../cidades/bom-jesus.js";
+
+    case "Itaú de Minas":
+      return "../cidades/itau.js";
+
+    case "São João Batista do Glória":
+      return "../cidades/gloria.js";
+
+    default:
+      return null;
   }
+
 }
 
-function continuar(){
+async function continuar(){
 
-  localStorage.setItem("tipoCadastro", tipoSelecionado);
-  localStorage.setItem("cidade", document.getElementById("cidade").value);
+  const tipo = document.querySelector('input[name="tipo"]:checked');
+  const cidade = document.getElementById("cidade").value;
+  const termos = document.getElementById("termos").checked;
 
-  if(tipoSelecionado === "cliente"){
+  if(!tipo){
+    alert("Selecione cliente ou motorista");
+    return;
+  }
+
+  if(!cidade){
+    alert("Selecione a cidade");
+    return;
+  }
+
+  if(!termos){
+    alert("Aceite os termos");
+    return;
+  }
+
+  const arquivo = getArquivoCidade(cidade);
+
+  if(!arquivo){
+    alert("Cidade inválida");
+    return;
+  }
+
+  await import(arquivo);
+
+  if(!cidadeConfig.ativa){
+    alert("Ainda não estamos na sua cidade 😔\nMas chegaremos em breve!");
+    return;
+  }
+
+  localStorage.setItem("cidade", cidade);
+  localStorage.setItem("tipo", tipo.value);
+
+  if(tipo.value === "cliente"){
     window.location.href = "cliente.html";
   }else{
     window.location.href = "motorista.html";
   }
+
 }
